b9e98d692a27f3b22c517301f9935b1569c18b00
Chase Wilson <contact@chasewilson.dev>
2020-03-30 13:42:32 -0500

    Added fuzzer

---
+++ fuzzing/src/rodeo.rs
@@ -0,0 +1,18 @@
+use honggfuzz::fuzz;
+use lasso::Rodeo;
+
+fn main() {
+    let mut rodeo = Rodeo::default();
+
+    loop {
+        fuzz!(|data: &[u8]| {
+            if let Ok(string) = std::str::from_utf8(data) {
+                if let Some(key) = rodeo.try_get_or_intern(string) {
+                    assert_eq!(string, rodeo.resolve(&key));
+                }
+            } else {
+                rodeo = Rodeo::default();
+            }
+        });
+    }
+}
